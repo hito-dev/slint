7c398e124aa43bed9d698bf5defe2e6e0165fb92
Misha Bunte <github@bunte.art>
2026-03-07 00:48:50 +0800

    Fix non-existing glyph for space

--- internal/compiler/passes/embed_glyphs.rs
+++ internal/compiler/passes/embed_glyphs.rs
@@ -456,11 +456,18 @@ fn generate_sdf_for_glyph(
     let face =
         fdsm_ttf_parser::ttf_parser::Face::parse(font.font.blob.data(), font.font.index).unwrap();
     let glyph_id = face.glyph_index(code_point).unwrap_or_default();
-    let mut shape = fdsm_ttf_parser::load_shape_from_face(&face, glyph_id)?;
-
     let metrics = sharedfontique::DesignFontMetrics::new(&font.font);
+
     let target_pixel_size = target_pixel_size as f64;
     let scale = target_pixel_size / metrics.units_per_em as f64;
+
+    let Some(mut shape) = fdsm_ttf_parser::load_shape_from_face(&face, glyph_id) else {
+        // For example, for space or emojis (which we don't handle as bitmap glyphs yet)
+        return Some(BitmapGlyph {
+            x_advance: (face.glyph_hor_advance(glyph_id).unwrap_or(0) as f64 * scale * 64.) as i16,
+            ..Default::default()
+        });
+    };
 
     // TODO: handle bitmap glyphs (emojis)
     let Some(bbox) = face.glyph_bounding_box(glyph_id) else {
